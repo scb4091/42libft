48d135f53c3da46de2aa56c488316963e951576c
Sergio Carruesco Bretos <scarrues@c2r5s4.42madrid.com>
2025-05-15 21:06:25 +0200

    Add

--- ft_split.c
+++ ft_split.c
@@ -46,12 +46,37 @@ static size_t	ft_split_num_c(char const *s, char c)
 	return (num_c);
 }
 
-char	**ft_split(char const *s, char c)
+static char	**ft_split_aux(char	**arr_ptr, char const *s, char c)
 {
 	size_t	ind;
 	size_t	num_str;
 	size_t	start_str;
-	size_t	new_word;
+
+	ind = 0;
+	num_str = 0;
+	start_str = 0;
+	while (s[ind])
+	{
+		if (s[ind] != c && ((ind == 0) || (s[ind - 1] == c)))
+			start_str = ind;
+		if (s[ind] != c && (s[ind + 1] == '\0' || s[ind + 1] == c))
+		{
+			arr_ptr[num_str] = ft_substr(s, start_str, (ind - start_str + 1));
+			if (!arr_ptr[num_str])
+			{
+				ft_split_free(arr_ptr, num_str);
+				return (NULL);
+			}
+			num_str++;
+		}
+		ind++;
+	}
+	arr_ptr[num_str] = NULL;
+	return(arr_ptr);
+}
+
+char	**ft_split(char const *s, char c)
+{
 	char	**arr_ptr;
 
 	if (!s)
@@ -59,39 +84,5 @@ char	**ft_split(char const *s, char c)
 	arr_ptr = (char **) malloc((ft_split_num_c(s, c) + 1) * sizeof(char *));
 	if (!arr_ptr)
 		return (NULL);
-	ind = 0;
-	num_str = 0;
-	new_word = 1;
-	while (s[ind])
-	{
-		if (s[ind] != c && new_word)
-		{
-			start_str = ind;
-			new_word = 0;
-		}
-		else if (s[ind] == c && !new_word)
-		{
-			arr_ptr[num_str] = ft_substr(s, start_str, (ind - start_str));
-			if (!arr_ptr[num_str])
-			{
-				ft_split_free(arr_ptr, num_str);
-				return (NULL);
-			}
-			num_str++;
-			new_word = 1;
-		}
-		ind++;
-	}
-	if (!new_word)
-	{
-		arr_ptr[num_str] = ft_substr(s, start_str, (ind - start_str));
-		if (!arr_ptr[num_str])
-		{
-			ft_split_free(arr_ptr, num_str);
-			return (NULL);
-		}
-		num_str++;
-	}
-	arr_ptr[num_str] = NULL;
-	return (arr_ptr);
+	return (ft_split_aux(arr_ptr, s, c));
 }
